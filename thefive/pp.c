6f57ec7acef939974947c23a71f15f159a8dda23
Swastik19Nit <122457803+Swastik19Nit@users.noreply.github.com>
2024-02-15 17:32:32 +0530

    Add files via upload

---
+++ thefive/pp.c
@@ -0,0 +1,15 @@
+void selection sort(int array[],int n)
+{
+    int index;
+    for(int i=0;i<n-1;i++)
+    {
+        index=i;
+        for(int j = i + 1; j < n; j++)
+        {
+            if array[j]<array[index]
+                index=j;
+        }
+        int temp = array[]
+
+    }
+}
